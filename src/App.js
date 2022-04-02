887ee22f6e3ccc936b52a823b3d357890b655d03
caihai <ch15519060282@163.com>
2022-04-02 10:48:52 +0800

    【新增】添加antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Button } from "antd";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="web">
+      <Button type="primary" loading="{false}">按钮</Button>
+    </div>
+  );
 }
 
 export default App;

29866a7419b41a5243ed2e497a6951a5e6968dad
caihai <ch15519060282@163.com>
2022-04-06 15:45:35 +0800

    【更新】动态渲染菜单图标

--- src/layout/index.js
+++ src/layout/index.js
@@ -3,7 +3,7 @@ import { Layout, Menu } from "antd";
 import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 import { Routes, Route } from "react-router-dom";
 import style from "./style.module.css";
-import routes from "../router/index"; // 路由表
+import routes from "../router/index"; // 前端路由表
 import axios from "../utils/axios";
 import MenuItem from './menu-item.js';
 
@@ -16,7 +16,7 @@ export default function LayoutViwe() {
   useEffect(() => {
     // 获取权限路由列表
     axios
-      .get("/mock-api/vue-clean-admin/get-menu-list.json")
+      .get("/mock-api/react-antd-admin/get-menu-list.json")
       .then((response) => {
         const { data } = response.data;
         setMenuList(data || []);
@@ -25,7 +25,7 @@ export default function LayoutViwe() {
 
   return (
     <Layout>
-      <Sider collapsed={collapsed}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className={style.logo} />
         <Menu theme="dark" mode="inline">
           {menuList.map((item) => MenuItem(item))}
--- src/layout/menu-item.js
+++ src/layout/menu-item.js
@@ -1,21 +1,23 @@
+import React from "react";
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
-import { MenuUnfoldOutlined } from "@ant-design/icons";
+import * as Icon from "@ant-design/icons";
+
+// 动态渲染icon
+function antdIcon(icon) {
+  return icon && React.createElement(Icon[icon]);
+}
 
 export default function MenuItem(item) {
   if (item.type === "1") {
     return (
-      <Menu.Item key={item.id} icon={<MenuUnfoldOutlined />} title={item.title}>
+      <Menu.Item key={item.id} icon={antdIcon(item.icon)}>
         <Link to={item.path}>{item.title}</Link>
       </Menu.Item>
     );
   } else {
     return (
-      <Menu.SubMenu
-        title={item.title}
-        key={item.id}
-        icon={<MenuUnfoldOutlined />}
-      >
+      <Menu.SubMenu title={item.title} key={item.id} icon={antdIcon(item.icon)}>
         {(item.children || []).map((i) => MenuItem(i))}
       </Menu.SubMenu>
     );
